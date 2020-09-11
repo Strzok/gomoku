ca2bcc0d341f1d88eb83ec8a9d8115a6f2da433c
Strzok <46719182+Strzok@users.noreply.github.com>
2020-09-11 15:02:31 +0200

    Update models.py

--- game/models.py
+++ game/models.py
@@ -1,18 +1,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+# from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
 class Coord_o(models.Model):
-	ip = models.GenericIPAddressField(primary_key=True)
-	coord_ox = models.CharField(max_length=200)
-	coord_oy = models.CharField(max_length=200)
+    """It keeps coordinates of computer move"""
+    ip = models.GenericIPAddressField(primary_key=True)
+    coord_ox = models.CharField(max_length=200, default=list)
+    coord_oy = models.CharField(max_length=200, default=list)
 class Coord_x(models.Model):
-	ip = models.GenericIPAddressField(primary_key=True)
-	coord_xx = models.CharField(max_length=200)
-	coord_xy = models.CharField(max_length=200)
+    """It keeps coordinates of player's move"""
+    ip = models.GenericIPAddressField(primary_key=True)
+    coord_xx = models.CharField(max_length=200, default=list)
+    coord_xy = models.CharField(max_length=200, default=list)
 class Difficulty_level(models.Model):
-	ip = models.GenericIPAddressField(primary_key=True)
-	level_in_model = models.CharField(max_length=200)	
+    """It keeps diffuculty level"""
+    ip = models.GenericIPAddressField(primary_key=True)
+    level_in_model = models.CharField(max_length=200, default=list)
+class Size(models.Model):
+    """It keeps size"""
+    ip = models.GenericIPAddressField(primary_key=True)
+    size_in_model = models.CharField(max_length=3, default=list)
